2a1d792b8ed078cb893172a1d0d8d2eb0c4e04a4
youssef-degachi <youssefdegachi0@gmail.com>
2024-10-22 10:43:46 +0100

    create connect with mysql

--- server.js
+++ server.js
@@ -13,4 +13,32 @@ app.listen(port);
 
 
 // MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'node_app'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+
+    if (result.length > 0) {
+      res.send('Login successful');
+    } else {
+      res.send('Invalid credentials');
+    }
+  });
+});
+
 
